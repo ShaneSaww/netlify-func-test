388746c6e97ed961acbeea58ab6cf6ef2ca747e8
Shane Smith-Sahnow <ShaneSaww@users.noreply.github.com>
2022-07-08 09:15:14 -0700

    Update js_hello.js

--- netlify/edge-functions/js_hello.js
+++ netlify/edge-functions/js_hello.js
@@ -1,6 +1,6 @@
 export default async (Request, Context) => {
-    Context.log(`Javscript Context Logging`);
-    console.log(`Javscript Console Logging`);
+    Context.log(`Javascript Context Logging`);
+    console.log(`Javascript Console Logging`);
     return new Response("Hello\n", {
       headers: { "content-type": "text/html" },
     });
