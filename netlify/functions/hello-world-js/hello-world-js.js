fa6a247052d19177df490bf35b6dec9fbca88755
Shane Smith-Sahnow <shane@netlify.com>
2022-07-20 14:27:42 -0700

    envs

--- netlify/functions/hello-world-js/hello-world-js.js
+++ netlify/functions/hello-world-js/hello-world-js.js
@@ -42,7 +42,7 @@ const handler = async (event) => {
     console.log('{"testFilter": "test"}')
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}\n ${process.env}` }),
+      body: JSON.stringify({ message: `Hello ${subject}\n ${JSON.stringify(process.env)}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
