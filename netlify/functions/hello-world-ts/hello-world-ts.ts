7da1e0eaeb95dfa90457b1ecf6325c15c4f89567
Shane Smith-Sahnow <ShaneSaww@users.noreply.github.com>
2022-10-27 13:22:32 -0700

    Update hello-world-ts.ts

--- netlify/functions/hello-world-ts/hello-world-ts.ts
+++ netlify/functions/hello-world-ts/hello-world-ts.ts
@@ -17,7 +17,7 @@ export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: `Netlify-Func-Test Hello, ${name}!`,
     }),
   }
 }
