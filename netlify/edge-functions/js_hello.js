43002a3efdca76443650e60e4fb7a690cc44ab57
Shane Smith-Sahnow <ShaneSaww@users.noreply.github.com>
2022-12-16 08:14:07 -0800

    asdf

--- netlify/edge-functions/js_hello.js
+++ netlify/edge-functions/js_hello.js
@@ -1,6 +1,8 @@
 export default async (Request, Context) => {
     Context.log(`Javascript Context Logging`);
     console.log(`Javascript Console Logging`);
+     console.warn(` warn level Javascript Console Logging`);
+    console.error(` error level Javascript Console Logging`);
     return new Response("Hello\n", {
       headers: { "content-type": "text/html" },
     });
